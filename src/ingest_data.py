271d9c37d5f97d913c8b0233a87b0053c643b94a
Ali <aliawan05500@gmail.com>
2025-07-12 23:21:44 +0500

    Ingest data file added

---
+++ src/ingest_data.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import logging
+import os
+
+
+class IngestData:
+    def __init__(self, data_path : str):
+        self.data_path = data_path
+        self.data
+
+
+    def load_data(self) -> pd.DataFrame:
+        try:
+            logging.info('Loading data from csv file')
+
+            if not os.path.exists(self.data_path):
+                raise FileNotFoundError(f"File Not found : {self.data_path}")
+            
+            self.data = pd.read_csv(self.data_path)
+            logging.info('Data loaded successfully')
+            return self.data
+        
+        except Exception as e:
+            logging.error(f'An error occurred : {str(e)}')
+            raise e
