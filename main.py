4e5aac9a4763b267f3ca6537f24adf17f827b8e1
Ali <aliawan05500@gmail.com>
2025-07-13 16:15:55 +0500

    some changes made in main and experiment file

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from src.ingest_data import IngestData
+from src.preprocess_data import Preprocessing
+from src.train_model import ModelTraining
 
 def func():
     raw_data_path = r'C:\Users\FINE\Electricity_cost_prediction\data\raw\electricity_cost_dataset.csv'
+    model_path = r'C:\Users\FINE\Electricity_cost_prediction\model'
 
 
     obj = IngestData(raw_data_path)
@@ -9,6 +12,14 @@ def func():
 
     print(data.head())
     print(data.info())
+
+    obj1 = Preprocessing(data)
+    process_data = obj1.preprocess_data()
+
+    print(process_data.head())
+
+    obj2 = ModelTraining(process_data, model_path)
+    obj2.train_model()
 
 if __name__ == "__main__":
     func()
